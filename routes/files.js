a78553dca5110e80266a08f4254109b4c29a56bf
taranbhatia201 <64377417+taranbhatia201@users.noreply.github.com>
2020-10-13 00:13:39 +0530

    updating server.js

--- routes/files.js
+++ routes/files.js
@@ -41,45 +41,4 @@ router.post("/",(req,res)=>{
 
 });
 
-router.post("/send",async (req,res)=>{
-  const {uuid , emailTo, emailFrom} = req.body;
-  // validate request
-  if(!uuid || !emailTo || !emailFrom){
-    return res.status(422).send({error : "All fields are required."});
-  }
-  // getting data from databse
-
-  const file = await File.findOne({uuid: uuid});
-  if(file.sender)
-  {
-    return res.status(422).send({error: 'Email already sent.'});
-  }
-
-  file.sender = emailFrom;
-  file.receiver = emailTo;
-
-  const response = await file.save();
-
-  // send email
-
-  const sendMail = require("../services/emailService");
-
-  sendMail({
-    from: emailFrom,
-    to: emailTo,
-    subject:'inShare file sharing',
-    text: `${emailFrom} shared a file with you.`,
-    html: require("../services/emailTemplate")({
-      emailFrom: emailFrom,
-      downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}`,
-      size: parseInt(file.size/1000) + 'KB',
-      expires: '24 hours'
-    })
-  });
-
-  return res.send({success: true});
-
-
-});
-
 module.exports = router;
